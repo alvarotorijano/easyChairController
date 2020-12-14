8797861504735d25b53ca9e93bcc8dfd2508a82f
Alvaro Torijano Garcia <alvarotorijano@gmail.com>
2020-12-14 01:11:54 +0100

    app wheel control working

--- easyChairController_.ino
+++ easyChairController_.ino
@@ -36,6 +36,8 @@
 
 BluetoothSerial SerialBT;
 
+#define START_FRAME         0xABCD       // [-] Start frme definition for reliable serial communication
+
 typedef struct {
   int8_t x;
   int8_t y;
@@ -50,10 +52,115 @@ union Transmission_buffer{
 bool lastBT_status;
 unsigned long trigger_open;
 
+// Global variables
+uint8_t idx = 0;                        // Index for new data pointer
+uint16_t bufStartFrame;                 // Buffer Start Frame
+byte *p;                                // Pointer declaration for the new received data
+byte incomingByte;
+byte incomingBytePrev;
+
+typedef struct{
+   uint16_t start;
+   int16_t  steer;
+   int16_t  speed;
+   uint16_t checksum;
+} SerialCommand;
+SerialCommand Command;
+
+typedef struct{
+   uint16_t start;
+   int16_t  cmd1;
+   int16_t  cmd2;
+   int16_t  speedR_meas;
+   int16_t  speedL_meas;
+   int16_t  batVoltage;
+   int16_t  boardTemp;
+   uint16_t cmdLed;
+   uint16_t checksum;
+} SerialFeedback;
+SerialFeedback Feedback;
+SerialFeedback NewFeedback;
+
+void hoverBoardSend(int16_t uSteer, int16_t uSpeed)
+{
+  // Create command
+  Command.start    = (uint16_t)START_FRAME;
+  Command.steer    = (int16_t)uSteer;
+  Command.speed    = (int16_t)uSpeed;
+  Command.checksum = (uint16_t)(Command.start ^ Command.steer ^ Command.speed);
+
+  // Write to Serial
+  Serial2.write((uint8_t *) &Command, sizeof(Command)); 
+  Serial.println("sending");
+}
+
+// ########################## RECEIVE ##########################
+void hoverBoardReceive()
+{
+  // Check for new data availability in the Serial buffer
+  if (Serial2.available()) {
+    incomingByte    = Serial2.read();                                 // Read the incoming byte
+    bufStartFrame = ((uint16_t)(incomingByte) << 8) | incomingBytePrev;   // Construct the start frame    
+  }
+  else {
+    return;
+  }
+
+  // If DEBUG_RX is defined print all incoming bytes
+  #ifdef DEBUG_RX
+    Serial.print(incomingByte);
+    return;
+  #endif      
+  
+  // Copy received data
+  if (bufStartFrame == START_FRAME) {                     // Initialize if new data is detected
+    p     = (byte *)&NewFeedback;
+    *p++  = incomingBytePrev;
+    *p++  = incomingByte;
+    idx   = 2;  
+  } else if (idx >= 2 && idx < sizeof(SerialFeedback)) {  // Save the new received data
+    *p++  = incomingByte; 
+    idx++;
+  } 
+  
+  // Check if we reached the end of the package
+  if (idx == sizeof(SerialFeedback)) {    
+    uint16_t checksum;
+    checksum = (uint16_t)(NewFeedback.start ^ NewFeedback.cmd1 ^ NewFeedback.cmd2 ^ NewFeedback.speedR_meas ^ NewFeedback.speedL_meas
+          ^ NewFeedback.batVoltage ^ NewFeedback.boardTemp ^ NewFeedback.cmdLed);
+  
+    // Check validity of the new data
+    if (NewFeedback.start == START_FRAME && checksum == NewFeedback.checksum) {
+      // Copy the new data
+      memcpy(&Feedback, &NewFeedback, sizeof(SerialFeedback));
+      
+      // Print data to built-in Serial
+      Serial.print("1: ");   Serial.print(Feedback.cmd1);
+      Serial.print(" 2: ");  Serial.print(Feedback.cmd2);
+      Serial.print(" 3: ");  Serial.print(Feedback.speedR_meas);
+      Serial.print(" 4: ");  Serial.print(Feedback.speedL_meas);
+      Serial.print(" 5: ");  Serial.print(Feedback.batVoltage);
+      Serial.print(" 6: ");  Serial.print(Feedback.boardTemp);
+      Serial.print(" 7: ");  Serial.println(Feedback.cmdLed);
+      
+    } else {
+      Serial.println("Non-valid data skipped");
+    }
+    idx = 0;  // Reset the index (it prevents to enter in this if condition in the next cycle)
+  }
+  
+  // Update previous states
+  incomingBytePrev  = incomingByte;
+}
+
+#define HOVER_SERIAL_BAUD   38400       // [-] Baud rate for Serial2 (used to communicate with the hoverboard)
+
 void setup() {
   Serial.begin(115200);
   SerialBT.begin("EasyChair"); //Bluetooth device name
+  Serial2.begin(HOVER_SERIAL_BAUD);
   Serial.println("The device started, now you can pair it with bluetooth!");
+  
 }
 
 uint8_t syncro1, syncro2;
@@ -68,6 +175,7 @@ void loop() {
         while (!SerialBT.available());
         last_transmission.dataReceived[i] = SerialBT.read();
       }
+      /*
       Serial.println(
       "BlinkL: " +          String((last_transmission.transmission.controls & LEFT_BLINKER_MASK) > 0) + " " +
       "BlinkR: " +          String((last_transmission.transmission.controls & RIGHT_BLINKER_MASK) > 0) + " " +
@@ -81,16 +189,19 @@ void loop() {
       "y: " + (int)last_transmission.transmission.y + " " +
       "status: " + String(last_transmission.transmission.controls)
       );
+      */
+      digitalWrite(TRIGGER_PIN, (trigger_open - millis()) > 0);
+      digitalWrite(LASER_PIN, last_transmission.transmission.controls & LASER_MASK);
+      digitalWrite(LEFT_BLINKER, !(last_transmission.transmission.controls & ( LEFT_BLINKER_MASK | EMERGENCY_BLINKER_MASK) && (millis() % BLINK_PERIOD > BLINK_PERIOD / 2)));
+      digitalWrite(RIGHT_BLINKER,!(last_transmission.transmission.controls & (RIGHT_BLINKER_MASK | EMERGENCY_BLINKER_MASK) && (millis() % BLINK_PERIOD > BLINK_PERIOD / 2)));
+
     }
     else {
       syncro1 = syncro2;
     }
   }
-  
-  digitalWrite(TRIGGER_PIN, (trigger_open - millis()) > 0);
-  digitalWrite(LASER_PIN, last_transmission.transmission.controls & LASER_MASK);
-  digitalWrite(LEFT_BLINKER, !(last_transmission.transmission.controls & ( LEFT_BLINKER_MASK | EMERGENCY_BLINKER_MASK) && (millis() % BLINK_PERIOD > BLINK_PERIOD / 2)));
-  digitalWrite(RIGHT_BLINKER,!(last_transmission.transmission.controls & (RIGHT_BLINKER_MASK | EMERGENCY_BLINKER_MASK) && (millis() % BLINK_PERIOD > BLINK_PERIOD / 2)));
+  hoverBoardSend(last_transmission.transmission.x * 10, sqrt(pow(last_transmission.transmission.x, 2) + pow(last_transmission.transmission.y, 2)) * 10);
+      
 
-  delay(20);
+  delay(50);
 }
